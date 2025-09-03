82d2be4ce9fadfc890bb8ef0b20724e7f59af7f1
InfinityCandy <la.salr.cdp@outlook.com>
2025-09-03 01:57:58 -0600

    Add missing model file

--- models/user.py
+++ models/user.py
@@ -1,6 +1,27 @@
 from extensions import db
+import enum
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.types import TypeDecorator, String
+
+
+class UserType(enum.Enum):
+    ADMIN = 'admin'
+    REGULAR = 'regular'
+
+
+class UserTypeEnum(TypeDecorator):
+    impl = String(10)
+
+    def process_bind_param(self, value, dialect):
+        if isinstance(value, UserType):
+            return value.value
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return None
+        return UserType(value)
 
 
 class User(UserMixin, db.Model):
@@ -9,6 +30,12 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
     first_name = db.Column(db.String(254), nullable=False)
     last_name = db.Column(db.String(254), nullable=False)
+
+    user_type = db.Column(
+        UserTypeEnum,
+        nullable=False,
+        default=UserType.REGULAR
+    )
 
     subscription = db.relationship(
         "Subscription",
